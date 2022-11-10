3c14f015086a25c45c65ba91795aacaad0d611ee
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-10 14:41:16 +0300

    ExceptionHandler implemented

---
+++ src/main/kotlin/com/gucardev/springbootkotlingraphql/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.gucardev.springbootkotlingraphql.exception
+
+import com.gucardev.springbootkotlingraphql.model.CustomError
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    fun userNotFoundException(ex: UserNotFoundException): ResponseEntity<CustomError> {
+        val errorMessage = CustomError(
+            HttpStatus.NOT_FOUND.value(),
+            ex.message
+        )
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage)
+    }
+}
