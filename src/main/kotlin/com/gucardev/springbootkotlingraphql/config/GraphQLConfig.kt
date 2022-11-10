7a35ccaf315ac9b0dc5ee79053d82899d4f3e1a9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-10 15:50:13 +0300

    GraphQlConfig created

---
+++ src/main/kotlin/com/gucardev/springbootkotlingraphql/config/GraphQLConfig.kt
@@ -0,0 +1,14 @@
+package com.gucardev.springbootkotlingraphql.config
+
+import graphql.scalars.ExtendedScalars
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.graphql.execution.RuntimeWiringConfigurer
+
+@Configuration
+class GraphQlConfig {
+    @Bean
+    fun runtimeWiringConfigurer(): RuntimeWiringConfigurer {
+        return RuntimeWiringConfigurer { wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.LocalTime) }
+    }
+}
