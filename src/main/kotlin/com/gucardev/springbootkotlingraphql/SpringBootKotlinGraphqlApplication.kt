f53fd8710b315227c1183904f4c0e7818e82de62
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-10 13:58:21 +0300

    Initial commit

---
+++ src/main/kotlin/com/gucardev/springbootkotlingraphql/SpringBootKotlinGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.gucardev.springbootkotlingraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinGraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinGraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/gucardev/springbootkotlingraphql/SpringBootKotlinGraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gucardev.springbootkotlingraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinGraphqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
