c2ddf7a20db3125bb72d1eabcc30f7fd8583c694
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-10 16:47:55 +0300

    Exception Handling bug fixed

--- src/main/kotlin/com/gucardev/springbootkotlingraphql/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/gucardev/springbootkotlingraphql/exception/GlobalExceptionHandler.kt
@@ -1,21 +1,27 @@
 package com.gucardev.springbootkotlingraphql.exception
 
-import com.gucardev.springbootkotlingraphql.model.CustomError
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import graphql.ErrorType
+import graphql.GraphQLError
+import graphql.GraphqlErrorBuilder
+import graphql.schema.DataFetchingEnvironment
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
+import org.springframework.stereotype.Component
+
+@Component
+class GlobalExceptionHandler : DataFetcherExceptionResolverAdapter() {
 
 
-@RestControllerAdvice
-class GlobalExceptionHandler {
-
-    @ExceptionHandler
-    fun userNotFoundException(ex: UserNotFoundException): ResponseEntity<CustomError> {
-        val errorMessage = CustomError(
-            HttpStatus.NOT_FOUND.value(),
-            ex.message
-        )
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage)
+    override fun resolveToSingleError(e: Throwable, env: DataFetchingEnvironment): GraphQLError? {
+        return when (e) {
+            is UserNotFoundException -> toGraphQLError(e)
+            is Exception -> toGraphQLError(e)
+            else -> super.resolveToSingleError(e, env)
+        }
     }
+
+    private fun toGraphQLError(e: Throwable): GraphQLError? {
+        return GraphqlErrorBuilder.newError().message(e.message).errorType(ErrorType.DataFetchingException).build()
+    }
+
+
 }
