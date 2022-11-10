b952880a28ef1670e0e490b7ed91142dfef0bf0a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-11-10 16:50:34 +0300

    crud operations added to UserService

--- src/main/kotlin/com/gucardev/springbootkotlingraphql/service/UserService.kt
+++ src/main/kotlin/com/gucardev/springbootkotlingraphql/service/UserService.kt
@@ -1,10 +1,12 @@
 package com.gucardev.springbootkotlingraphql.service
 
 import com.gucardev.springbootkotlingraphql.converter.toUSER
+import com.gucardev.springbootkotlingraphql.converter.toUser
 import com.gucardev.springbootkotlingraphql.exception.UserNotFoundException
 import com.gucardev.springbootkotlingraphql.model.User
 import com.gucardev.springbootkotlingraphql.repository.UserRepository
 import com.gucardev.springbootkotlingraphql.request.UserCreateRequest
+import com.gucardev.springbootkotlingraphql.request.UserUpdateRequest
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,5 +18,15 @@ class UserService(private val userRepository: UserRepository) {
 
     fun getUserByID(id: Long): User = userRepository.findById(id)
         .orElseThrow { UserNotFoundException("user not found!") }
+
+    fun deleteUser(id: Long) {
+        val existing = getUserByID(id)
+        userRepository.delete(existing)
+    }
+
+    fun updateUser(updateUserRequest: UserUpdateRequest): User {
+        val existing = getUserByID(updateUserRequest.id)
+        return  userRepository.save(updateUserRequest.toUser(existing))
+    }
 
 }
